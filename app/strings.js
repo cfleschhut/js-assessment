b0dae398614e9467b027a635f0b522f0bd8a0924
Christian Fleschhut <weltrekordhalter@gmail.com>
2016-01-12 15:41:47 +0100

    add string answers

--- app/strings.js
+++ app/strings.js
@@ -1,13 +1,24 @@
 exports = (typeof window === 'undefined') ? global : window;
 
 exports.stringsAnswers = {
-  reduceString: function(str, amount) {
 
+  reduceString: function(str, amount) {
+    var newArr = [];
+    var group = _.groupBy(str.split(''), function(char) {
+      return char;
+    });
+    for (var i in group) {
+      newArr.push(group[i].splice(0, amount).join(''));
+    }
+    return newArr.join('');
   },
+
   wordWrap: function(str, cols) {
 
   },
-  reverseString: function(str) {
 
+  reverseString: function(str) {
+    return str.split('').reverse().join('');
   }
+
 };
